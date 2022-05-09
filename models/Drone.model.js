921e2c7900c1cd1843d41672aae6736f3eb98802
Pelayo Trives <pelayotrives@gmail.com>
2022-05-09 16:53:27 +0200

    Commit inicial.

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,17 @@
-// Iteration #1
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const droneSchema = new Schema({
+    name: {
+        type: String
+    },
+    propellers: {
+        type: Number
+    },
+    maxSpeed: {
+        type: Number
+    }
+})
+
+const droneModel = model("drone", droneSchema);
+module.exports = droneModel;
